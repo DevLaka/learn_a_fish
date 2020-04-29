52e8ce7dfbdd90401fec1cbcc906965b453a4581
Lakshan Bandara <lakshanwarunab@gmail.com>
2020-04-29 23:51:46 +0530

    fixed does not navigate to register

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -2,127 +2,115 @@ import 'package:flutter/material.dart';
 import 'package:learnafish/services/authentication_services/AuthenticationService.dart';
 
 class login extends StatefulWidget {
-  
   final Function redirect;
-  login({ this.redirect });
+  login({this.redirect});
 
   @override
   _loginState createState() => _loginState();
 }
 
 class _loginState extends State<login> {
- 
   final services _authentication = services();
-    //for validations
+  //for validations
   final _key = GlobalKey<FormState>();
   String error = '';
-    //text fields for  login
+  //text fields for  login
   String email = '';
-  String password='';
+  String password = '';
   @override
   Widget build(BuildContext context) {
-        return new Scaffold(
-           appBar: AppBar(
+    return new Scaffold(
+      appBar: AppBar(
         title: Text('Sea World!'),
         actions: <Widget>[
           FlatButton.icon(
-            icon: Icon(Icons.person), 
-            label: Text('Register'),
-             onPressed: (){
-              widget.redirect();
-             }
-            )
+              icon: Icon(Icons.person),
+              label: Text('Register'),
+              onPressed: () {
+                widget.redirect();
+              })
         ],
       ),
-    body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-            child:Stack(
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                    child: Text(
-                        'Welcome To SEAWORLD!',
-                        style: TextStyle(
-                            fontSize: 20.0,fontWeight:FontWeight.bold)
-                    )
-                ),
-                Container(
-                   padding: EdgeInsets.fromLTRB(15.0, 150.0, 15.0, 0.0),
-                   child: Form(
-                     key: _key,
-                     child: Column(
-                       children: <Widget>[
-                         SizedBox(height:20.0),
-                         TextFormField(
-                           validator: (val) =>val.isEmpty ? 'Enter email' : null,
-                           decoration: InputDecoration(
-                               labelText : 'Email',
-                             labelStyle : TextStyle(
-                               fontFamily : 'Montserrat',
-                               fontWeight : FontWeight.bold,
-                               color : Colors.black
-                             )
-                           ),
-                           onChanged: (val){
-                             setState(()=>email = val);
-                           }
-                         ),
-                         SizedBox(height: 20.0,),
-                         TextFormField(
-                            validator: (val) =>val.length < 5 ? 
-                           'Enter a password more than 5 characters' : null,
-                           decoration: InputDecoration(
-                             labelText : 'Password',
-                             labelStyle : TextStyle(
-                               fontFamily : 'Montserrat',
-                               fontWeight : FontWeight.bold,
-                               color : Colors.black
-                             )
-                           ),
-                           obscureText: true,
-                           onChanged: (val){
-                               setState(()=>password = val);
-                           },
-                         ),
-                          SizedBox(height: 20.0,),
-                          RaisedButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius : BorderRadius.circular(20.0),
-                            ),
-                              color: Colors.lightBlue[400] ,
-                              child: Text(
-                                'Sign In'
-                              ),
-                              onPressed: () async{
-                                   if(_key.currentState.validate()){
-                                    dynamic output = await _authentication.login(email, password);
-                                    if(output == null){
-                                      setState(() => error= 'incorret password or email !');
-
-                                    }
-                                  }
-                              }
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              child: Stack(
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text('Welcome To SEAWORLD!',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.bold))),
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 150.0, 15.0, 0.0),
+                child: Form(
+                  key: _key,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter email' : null,
+                          decoration: InputDecoration(
+                              labelText: 'Email',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black)),
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          }),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        validator: (val) => val.length < 5
+                            ? 'Enter a password more than 5 characters'
+                            : null,
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black)),
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
                           ),
-                           SizedBox(height:12.0),
-                          Text(
-                              error,
-                              style:TextStyle(color:Colors.red, fontSize: 12.0),
-                          )
-                       ],
-                     ),
-                   ),
-                )
-               
-              ],
-            )
-        )
-      ],
-    ),
-
-  );
+                          color: Colors.lightBlue[400],
+                          child: Text('Sign In'),
+                          onPressed: () async {
+                            if (_key.currentState.validate()) {
+                              dynamic output =
+                                  await _authentication.login(email, password);
+                              if (output == null) {
+                                setState(() =>
+                                    error = 'incorret password or email !');
+                              }
+                            }
+                          }),
+                      SizedBox(height: 12.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 12.0),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ))
+        ],
+      ),
+    );
   }
 }
-
- 
--- lib/screens/Register.dart
+++ lib/screens/Register.dart
@@ -2,27 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:learnafish/services/authentication_services/AuthenticationService.dart';
 
 class register extends StatefulWidget {
-
   final Function redirect;
-  register({ this.redirect });
+  register({this.redirect});
 
   @override
   _registerState createState() => _registerState();
 }
 
 class _registerState extends State<register> {
-  
-   final services _authentication = services();
+  final services _authentication = services();
 
   //for validations
   final _key = GlobalKey<FormState>();
   String error = '';
 
-
-
-    //text fields for  login
+  //text fields for  login
   String registeremail = '';
-  String registerpassword='';
+  String registerpassword = '';
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -30,94 +26,88 @@ class _registerState extends State<register> {
         title: Text('Register'),
         actions: <Widget>[
           FlatButton.icon(
-            icon: Icon(Icons.person), 
-             label: Text('Login'),
-            onPressed: (){
-              widget.redirect();
-            }
-            )
+              icon: Icon(Icons.person),
+              label: Text('Login'),
+              onPressed: () {
+                widget.redirect();
+              })
         ],
       ),
-    body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-            child:Stack(
-              children: <Widget>[
-
-                Container(
-                   padding: EdgeInsets.fromLTRB(15.0, 110.0, 15.0, 0.0),
-                   child: Form(
-                     key: _key,
-                     child: Column(
-                       children: <Widget>[
-                         SizedBox(height:20.0),
-                         TextFormField(
-                           validator: (val) =>val.isEmpty ? 'Enter email' : null,
-                           decoration: InputDecoration(
-                               labelText : 'Email',
-                             labelStyle : TextStyle(
-                               fontFamily : 'Montserrat',
-                               fontWeight : FontWeight.bold,
-                               color : Colors.black
-                             )
-                           ),
-                           onChanged: (val){
-                             setState(()=>registeremail = val);
-                           }
-                         ),
-                         SizedBox(height: 20.0,),
-                         TextFormField(
-                           validator: (val) =>val.length < 5 ? 
-                           'Enter a password more than 5 characters' : null,
-                           decoration: InputDecoration(
-                             labelText : 'Password',
-                             labelStyle : TextStyle(
-                               fontFamily : 'Montserrat',
-                               fontWeight : FontWeight.bold,
-                               color : Colors.black
-                             )
-                           ),
-                           obscureText: true,
-                           onChanged: (val){
-                               setState(()=>registerpassword = val);
-                           },
-                         ),
-                          SizedBox(height: 20.0,),
-                          RaisedButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius : BorderRadius.circular(20.0),
-                            ),
-                              color: Colors.lightBlue[400] ,
-                              child: Text(
-                                'Register'
-                              ),
-                              onPressed: () async{
-                                  if(_key.currentState.validate()){
-                                    dynamic output = await _authentication.register(registeremail, registerpassword);
-                                    if(output == null){
-                                      setState(() => error= 'registration unsuccessfull');
-
-                                    }
-                                  }
-                              }
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 110.0, 15.0, 0.0),
+                child: Form(
+                  key: _key,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter email' : null,
+                          decoration: InputDecoration(
+                              labelText: 'Email',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black)),
+                          onChanged: (val) {
+                            setState(() => registeremail = val);
+                          }),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        validator: (val) => val.length < 5
+                            ? 'Enter a password more than 5 characters'
+                            : null,
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black)),
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => registerpassword = val);
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
                           ),
-                          SizedBox(height:12.0),
-                          Text(
-                              error,
-                              style:TextStyle(color:Colors.red, fontSize: 12.0),
-                          )
-                       ],
-                     ),
-                   ),
-                )
-               
-              ],
-            )
-        )
-      ],
-    ),
-
-  );
+                          color: Colors.lightBlue[400],
+                          child: Text('Register'),
+                          onPressed: () async {
+                            if (_key.currentState.validate()) {
+                              dynamic output = await _authentication.register(
+                                  registeremail, registerpassword);
+                              if (output == null) {
+                                setState(
+                                    () => error = 'registration unsuccessfull');
+                              }
+                            }
+                          }),
+                      SizedBox(height: 12.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.red, fontSize: 12.0),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ))
+        ],
+      ),
+    );
   }
 }
